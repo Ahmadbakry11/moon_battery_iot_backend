3f7484edc185145f93ecc499b7a2da85eb01fafb
Ahmad Bakry <ahmadbakry.gad@gmail.com>
2024-11-03 15:32:18 +0200

    Test: Add authentication test cases
    - Test against authenticated and unauthenticated batteries making requests
    
    - Add battery auth_token in the Auth Headers for authenticated battery

--- spec/requests/battery_configurations/update_spec.rb
+++ spec/requests/battery_configurations/update_spec.rb
@@ -1,61 +1,74 @@
 # frozen_string_literal: true
 
 RSpec.describe BatteryConfigurationsController, type: :request do
-  describe 'PUT /moon_batteries/:serial_number/configurations' do
+  describe 'configurations endpoint' do
     let!(:moon_battery) { create :moon_battery }
     let!(:serial_number) { moon_battery.serial_number }
-
-    before { put "/moon_batteries/#{serial_number}/configurations", params: params }
-
-    context 'when the provided moon_battery serial number exists' do
-      context 'when the provided params are all valid' do
-        let!(:params) do
-          {
-            configurations: [
-              { config_name: 'setting1', config_value: 'value1' },
-              { config_name: 'setting2', config_value: 'value2' }
-            ]
-          }
-        end
-
-        it 'returns status of no_content' do
-          expect(response).to have_http_status(:no_content)
-        end
-
-        it 'saves the configurations' do
-          expect(moon_battery.battery_configurations.find_by(config_name: 'setting1').config_value).to eq('value1')
-          expect(moon_battery.battery_configurations.find_by(config_name: 'setting2').config_value).to eq('value2')
-        end
-      end
-
-      context 'when the provided params are all invalid' do
-        let!(:params) do
-          {
-            configurations: [
-              { config_name: '', config_value: 'value1' }
-            ]
-          }
-        end
-
-        it 'does not save the configurations and return an error' do
-          expect(response).to have_http_status(:unprocessable_entity)
-        end
-      end
+    let!(:params) do
+      {
+        configurations: [
+          { config_name: 'setting1', config_value: 'value1' },
+          { config_name: 'setting2', config_value: 'value2' }
+        ]
+      }
     end
 
-    context 'when the provided moon_battery serial number does not exist' do
-      let!(:serial_number) { 'invalid_serail_number' }
+    describe 'PUT /moon_batteries/:serial_number/configurations' do
+      context 'when the battery making the request is authenticated' do
+        before { put "/moon_batteries/#{serial_number}/configurations", params: params, headers: { 'Authorization' => moon_battery.auth_token } }
 
-      let!(:params) do
-        {
-          configurations: [
-            { config_name: 'setting1', config_value: 'value1' }
-          ]
-        }
+        context 'when the provided moon_battery serial number exists' do
+          context 'when the provided params are all valid' do
+            it 'returns status of no_content' do
+              expect(response).to have_http_status(:no_content)
+            end
+    
+            it 'saves the configurations' do
+              expect(moon_battery.battery_configurations.find_by(config_name: 'setting1').config_value).to eq('value1')
+              expect(moon_battery.battery_configurations.find_by(config_name: 'setting2').config_value).to eq('value2')
+            end
+          end
+    
+          context 'when the provided params are all invalid' do
+            let(:params) do
+              {
+                configurations: [
+                  { config_name: '', config_value: 'value1' }
+                ]
+              }
+            end
+    
+            it 'does not save the configurations and return an error' do
+              expect(response).to have_http_status(:unprocessable_entity)
+            end
+          end
+        end
+    
+        context 'when the provided moon_battery serial number does not exist' do
+          let!(:serial_number) { 'invalid_serail_number' }
+        
+          it 'returns a not_found error' do
+            expect(response).to have_http_status(:not_found)
+          end
+        end
       end
 
-      it 'returns a not_found error' do
-        expect(response).to have_http_status(:not_found)
+      context 'when the battery making the request is not authenticated' do
+        context 'When the Authorization header is missing' do
+          before { put "/moon_batteries/#{serial_number}/configurations", params: params }
+        
+          it 'returns unauthorized status' do
+            expect(response).to have_http_status(:unauthorized)
+          end
+        end
+
+        context 'When the Authorization header is existing with invalid token' do
+          before { put "/moon_batteries/#{serial_number}/configurations", params: params, headers: { 'Authorization' => 'invalid_token' } }
+        
+          it 'returns unauthorized status' do
+            expect(response).to have_http_status(:unauthorized)
+          end
+        end
       end
     end
   end
--- spec/requests/moon_batteries/ping_spec.rb
+++ spec/requests/moon_batteries/ping_spec.rb
@@ -1,27 +1,49 @@
 # frozen_string_literal: true
 
 RSpec.describe MoonBatteriesController, type: :request do
-  describe 'PUT /moon_batteries/:serial_number/ping' do
+  describe 'ping endpoint' do
     let!(:moon_battery) { create :moon_battery }
     let!(:serial_number) { moon_battery.serial_number }
+    
+    describe 'PUT /moon_batteries/:serial_number/ping' do
+      context 'when the battery making the request is authenticated' do
+        before { put "/moon_batteries/#{serial_number}/ping", headers: { 'Authorization' => moon_battery.auth_token } }
 
-    before { put "/moon_batteries/#{serial_number}/ping" }
-
-    context 'when the target moon battery exists, i.e. valid serial_number' do
-      it 'returns no_content status' do
-        expect(response).to have_http_status(:no_content)
+        context 'when the target moon battery exists, i.e. valid serial_number' do
+          it 'returns no_content status' do
+            expect(response).to have_http_status(:no_content)
+          end
+    
+          it 'touches the current battery and updates the updated_at time' do
+            expect(moon_battery.reload.last_contact_at).to be_within(1.second).of(Time.current)
+          end
+        end
+    
+        context 'when the target moon battery does not exists, i.e. invalid serial_number' do
+          let(:serial_number) { 'invalid_number' }
+    
+          it 'returns not_found status error' do
+            expect(response).to have_http_status(:not_found)
+          end
+        end
       end
 
-      it 'touches the current battery and updates the updated_at time' do
-        expect(moon_battery.reload.last_contact_at).to be_within(1.second).of(Time.current)
-      end
-    end
+      context 'when the battery making the request is not authenticated' do
+        context 'When the Authorization header is missing' do
+          before { put "/moon_batteries/#{serial_number}/ping" }
+        
+          it 'returns unauthorized status' do
+            expect(response).to have_http_status(:unauthorized)
+          end
+        end
 
-    context 'when the target moon battery does not exists, i.e. invalid serial_number' do
-      let(:serial_number) { 'invalid_number' }
-
-      it 'returns not_found status error' do
-        expect(response).to have_http_status(:not_found)
+        context 'When the Authorization header is existing with invalid token' do
+          before { put "/moon_batteries/#{serial_number}/ping", headers: { 'Authorization' => 'invalid_token' } }
+        
+          it 'returns unauthorized status' do
+            expect(response).to have_http_status(:unauthorized)
+          end
+        end
       end
     end
   end
