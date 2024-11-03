948e8957141227d6f7341292bf5d0c8f306a5942
Ahmad Bakry <ahmadbakry.gad@gmail.com>
2024-11-03 16:17:00 +0200

    Feat: Add auth_token in MonnBattery
    - Add validations for auth_token

--- app/models/moon_battery.rb
+++ app/models/moon_battery.rb
@@ -4,11 +4,12 @@ class MoonBattery < ApplicationRecord
   MAC_ADDRESS_FORMAT = /\A([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})\Z/
 
   has_secure_token :serial_number
+  has_secure_token :auth_token
 
   has_many :battery_configurations, dependent: :destroy
 
-  validates :mac_address, :serial_number, presence: true
-  validates :mac_address, :serial_number, uniqueness: true
+  validates :mac_address, :serial_number, :auth_token, presence: true
+  validates :mac_address, :serial_number, :auth_token, uniqueness: true
   validates :mac_address, format: { with: MAC_ADDRESS_FORMAT, message: 'invalid MAC address' }
 
   def ping
